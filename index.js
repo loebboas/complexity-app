edd99918b2ca6d6d4e3fc284cf9d3afb4bbf9b0d
Boas Loeb <loebboas@gmail.com>
2017-11-06 09:43:48 +0100

    basic node app

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-var express = require('express');
-var app = express();
+var http = require('http');
 
-app.set('port', (process.env.PORT || 5000));
+http.createServer(function(request, response) {
+	response.writeHead(200);
+	response.write("Hello, this is dog");
+	response.end();
+}).listen(8080);
 
-app.use(express.static(__dirname + '/src'));
-
-app.get('/', function(request, response) {
-  res.set('Content-Type', 'text/html')
-  	.sendFile(path.join(__dirname, '/src/index.html'))
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+console.log('Node app is running on port', app.get('port'));
