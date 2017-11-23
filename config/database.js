93831377ac1683b1c4d8eef24562d4e88338c2a0
jvamvas <jvamvas@users.noreply.github.com>
2017-11-23 20:10:47 +0100

    Use fixed secret key if available

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
-const crypto = require('crypto').randomBytes(256).toString('hex');
+const crypto = require('crypto');
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/complexity-app-db';
 const MONGODB_NAME = process.env.MONGODB_NAME || 'complexity-app-db';
+const SECRET_KEY = process.env.SECRET_KEY || crypto.randomBytes(256).toString('hex');
 
 module.exports = {
     uri: MONGODB_URI,
-    secret: crypto,
     db: MONGODB_NAME,
+    secret: SECRET_KEY,
 };
