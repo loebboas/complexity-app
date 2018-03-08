e516cd312d79fea95048c2c7d8886df5f0cac292
Boas Loeb <loebboas@gmail.com>
2018-03-08 18:24:20 +0100

    v.0.0.2a

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -7,23 +7,54 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
+  activePerspective; Früchte
+  activeContext; Baum
+  activeThought; Apfel
 
-	hardThoughts = [
+	hardThought = [
 	{id: 1, label: 'Complexity-App', context: [{id: 1}], perspective: [{id: 2}, {id: 3}, {id: 4}], meaning: []},
+  {id: 2, label: 'Welcome', context: [{id: 1}], perspective: [{id: 2}], meaning: [{ id: 5}, { id: 6}]},
+  {id: 3, label: 'Public', context: [{id: 1}], perspective: [{id: 3}], meaning: [{ id: 7}]},
+  {id: 4, label: 'About', context: [{id: 1}], perspective: [{id: 4}], meaning: [{ id: 8 }]},
+  {id: 5, label: 'Login', context: [{id: 2}, {id: 1}], perspective: [{id: 2}], meaning: []},
+  {id: 6, label: 'Register', context: [{id: 2}, {id: 1}], perspective: [{id: 2}], meaning: []},
+  {id: 7, label: 'Public Thoughts', context: [{id: 2}, {id: 1}], perspective: [{id: 2}], meaning: []},
+  {id: 8, label: 'Text about this Homepage', context: [{id: 1}, {id: 1}], perspective: [{id: 2}], meaning: []},
+  ];
+
+   //1. Lade Objekt: Complexity-App + alle Objekte in Context in Array Contexte, alle Perspectiven in Array Perspectives etc.
+
+
+  chosenContext = {id: 1, label: 'Complexity-App', context: [{id: 1}], perspective: [{id: 2}, {id: 3}, {id: 4}], meaning: []};
+  chosenPerspective = {id: 2, label: 'Welcome', context: [{id: 1}], perspective: [{id: 2}], meaning: [{ id: 5}, { id: 6}]};
+
+  allContexts = [ {id: 1, label: 'Complexity-App', context: [{id: 1}], perspective: [{id: 2}, {id: 3}, {id: 4}], meaning: []} ];
+  allPerspectives = [    
     {id: 2, label: 'Welcome', context: [{id: 1}], perspective: [{id: 2}], meaning: [{ id: 5}, { id: 6}]},
     {id: 3, label: 'Public', context: [{id: 1}], perspective: [{id: 3}], meaning: [{ id: 7}]},
-    {id: 4, label: 'About', context: [{id: 1}], perspective: [{id: 4}], meaning: [{ id: 8 }]},
+    {id: 4, label: 'About', context: [{id: 1}], perspective: [{id: 4}], meaning: [{ id: 8 }]}
+    ];
+
+  //2. Lade alle Objekte für die gilt: Context = ChosenContext && Perspective = ChosenPerspective
+  allMeanings = [
     {id: 5, label: 'Login', context: [{id: 2}, {id: 1}], perspective: [{id: 2}], meaning: []},
-    {id: 6, label: 'Register', context: [{id: 2}, {id: 1}], perspective: [{id: 2}], meaning: []},
-    {id: 7, label: 'Public Thoughts', context: [{id: 2}, {id: 1}], perspective: [{id: 2}], meaning: []},
-    {id: 8, label: 'Text about this Homepage', context: [{id: 1}, {id: 1}], perspective: [{id: 2}], meaning: []},
-  	];
+    {id: 6, label: 'Register', context: [{id: 2}, {id: 1}], perspective: [{id: 2}], meaning: []}
+    ];
+  
+
 	showFiller = false;
 
-  constructor() { }
+  constructor() {  }
+
+  EditMid() {
+  	if(this.editMid == false) {
+  		this.editMid = true;
+  	} else {
+  		this.editMid = false;
+  	}
+  }
 
   ngOnInit() {
-
   	//1. Check URL for Perspective
   	//2. If no URL: Check if LoggedIn
   	//2.a If LoggedIn --> Load "My-Room"
